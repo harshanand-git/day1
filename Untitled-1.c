9e7193fc5533920db28a9107623142b6cfe40ecd
harshanand-git <harshanand410@gmail.com>
2025-08-20 08:46:20 +0530

    ist change

---
+++ Untitled-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+void main() 
+{  
+ int height,base;
+  float area; 
+    printf("enter height and base:");
+       scanf("%d%d",& height,&base);
+        area=0.5*height*base;
+         printf("area of triangle=%f",area); 
+} 
